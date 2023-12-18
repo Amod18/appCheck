264c9460a644f2ee25c53c03c1a2f3ada050d796
Amod Nagpal <amodnagpal46@gmail.com>
2023-12-18 16:33:33 +0530

    App ready

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React from "react";
 import Chatbot from "react-simple-chatbot";
-import { ThemeProvider } from 'styled-components';
 import steps from './data/steps.json';
-import logo from './CMSlogo.png'
+import './App.css'
 
-const theme = {
-  background: '#C9FF8F',
-  headerBgColor: '#197B22',
-  headerFontSize: '20px',
-  botBubbleColor: '#0F3789',
-  headerFontColor: 'white',
-  botFontColor: 'white',
-  userBubbleColor: '#FF5733',
-  userFontColor: 'black',
+// const theme = {
+//   background: '#C9FF8F',
+//   headerBgColor: '#197B22',
+//   headerFontSize: '20px',
+//   botBubbleColor: '#0F3789',
+//   headerFontColor: 'white',
+//   botFontColor: 'white',
+//   userBubbleColor: '#FF5733',
+//   userFontColor: 'black',
+// };
+
+const chatbotStyle = {
+  width: '100%', // Occupy full width
+  height: '100vh ! important', // Occupy full height of the viewport
 };
 
 export default function Temp() {
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <Chatbot
-          recognitionEnable={true} // --> For giving the voice inputs
-          botAvatar={logo} // --> Bot logo.
-          headerTitle="Chatbot" // --> Title for the chat.
-          steps={steps} // --> Passing the json file in the chatbot component.
-        />
-      </ThemeProvider>
-    </>
+    <div style={{ width: '100%', height: '100vh ! important' }}>
+      <Chatbot
+        steps={steps}
+        style={chatbotStyle}
+      />
+    </div>
   );
 }
